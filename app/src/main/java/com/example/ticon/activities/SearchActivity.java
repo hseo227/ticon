6fba08a23d6d7897f27ab0c211cd3681cae12619
jjun273 <jjun273@aucklanduni.ac.nz>
2022-08-19 23:09:42 +1200

    Implemented filter option to sort search results.

--- app/src/main/java/com/example/ticon/activities/SearchActivity.java
+++ app/src/main/java/com/example/ticon/activities/SearchActivity.java
@@ -1,6 +1,10 @@
 package com.example.ticon.activities;
 
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+import android.widget.SearchView;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
@@ -16,12 +20,12 @@ public class SearchActivity extends AppCompatActivity {
 
     // Arraylist for storing data
     private ArrayList<ListModel> listModelArrayList;
+    private EmoticonAdapter adapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_search);
-        ListRV = findViewById(R.id.listRView);
 
         // here we have created new array list and added data to it.
         listModelArrayList = new ArrayList<>();
@@ -33,21 +37,56 @@ public class SearchActivity extends AppCompatActivity {
         listModelArrayList.add(new ListModel(R.drawable.close_icon, R.drawable.close_icon, R.drawable.close_icon, "Close icon"));
         listModelArrayList.add(new ListModel(R.drawable.search_icon, R.drawable.search_icon, R.drawable.search_icon, "Search icon"));
 
-        // we are initializing our adapter class and passing our arraylist to it.
-        EmoticonAdapter emoticonAdapter = new EmoticonAdapter(this, listModelArrayList);
+        RecyclerView recyclerView = findViewById(R.id.listRView);
+        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(this);
+        adapter = new EmoticonAdapter(this, listModelArrayList);
 
-        // below line is for setting a layout manager for our recycler view.
-        // here we are creating vertical list so we will provide orientation as vertical
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        recyclerView.setLayoutManager(layoutManager);
+        recyclerView.setAdapter(adapter);
 
-        // in below two lines we are setting layoutmanager and adapter to our recycler view.
-        ListRV.setLayoutManager(linearLayoutManager);
-        ListRV.setAdapter(emoticonAdapter);
+//                ListRV = findViewById(R.id.listRView);
 
-        ItemSpacing itemSpacing = new ItemSpacing(16);
-        ListRV.addItemDecoration(itemSpacing);
+//        // we are initializing our adapter class and passing our arraylist to it.
+//        EmoticonAdapter emoticonAdapter = new EmoticonAdapter(this, listModelArrayList);
+//
+//        // below line is for setting a layout manager for our recycler view.
+//        // here we are creating vertical list so we will provide orientation as vertical
+//        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+//
+//        // in below two lines we are setting layoutmanager and adapter to our recycler view.
+//        ListRV.setLayoutManager(linearLayoutManager);
+//        ListRV.setAdapter(emoticonAdapter);
+//
+//        // spacing between each cardview
+//        ItemSpacing itemSpacing = new ItemSpacing(16);
+//        ListRV.addItemDecoration(itemSpacing);
 
     }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater inflater = getMenuInflater();
+        inflater.inflate(R.menu.main_menu, menu);
+
+        MenuItem item = menu.findItem(R.id.action_search);
+        SearchView searchView = (SearchView) item.getActionView();
+
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String s) {
+                return true;
+            }
+
+            @Override
+            public boolean onQueryTextChange(String s) {
+                adapter.getFilter().filter(s);
+                return true;
+            }
+        });
+        return true;
+
+    }
+
 }
 
 
