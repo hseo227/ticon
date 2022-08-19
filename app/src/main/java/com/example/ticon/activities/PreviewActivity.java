ef4db35934b24150864453a0b025f898a856fd7a
ykim273 <ykim273@aucklanduni.ac.nz>
2022-08-19 19:52:36 +1200

    Add PreviewActivity.java
    It was in gitignore

---
+++ app/src/main/java/com/example/ticon/activities/PreviewActivity.java
@@ -0,0 +1,17 @@
+package com.example.ticon.activities;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+
+import com.example.ticon.R;
+
+public class PreviewActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_preview);
+    }
+
+}
