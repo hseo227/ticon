4dcc3e9a8250f30eec93c6caacc480ac4930d8a6
Hayoon Seo <hseo227@aucklanduni.ac.nz>
2022-08-20 18:58:02 +1200

    Commiting to checkout to activities.

--- app/src/main/java/com/example/ticon/data/DataProvider.java
+++ app/src/main/java/com/example/ticon/data/DataProvider.java
@@ -1,4 +1,29 @@
 package com.example.ticon.data;
 
+import com.example.ticon.models.Category;
+
+import java.util.ArrayList;
+import java.util.List;
+
 public class DataProvider {
+
+    public static void getEmoticonData(CompleteListener<List<Category>> onCompleteListener){
+        List<Category> emoticonList = new ArrayList<>();
+
+        FirebaseFirestore db = FirebaseFirestore.getInstance();
+
+        db.collection("funny").get().addOnCompleteListener(task -> {
+            if (task.isSuccessful()) {
+                QuerySnapshot results = task.getResult();
+                for (Emoticons emoticons: results.toObjects(Emoticons.class)) {
+                    emoticonList.add(coffeeBeans);
+                }
+            }
+
+            if (emoticonList.size() > 0) {
+                onCompleteListener.onComplete(emoticonList);
+            }
+        });
+    }
+
 }
