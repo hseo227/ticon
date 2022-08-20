01a37945a5f8eeda8829a951066ecc42f1067f01
ykim273 <ykim273@aucklanduni.ac.nz>
2022-08-20 22:38:45 +1200

    Update DetailsActivicty.java with button

--- app/src/main/java/com/example/ticon/activities/DetailsActivity.java
+++ app/src/main/java/com/example/ticon/activities/DetailsActivity.java
@@ -2,7 +2,10 @@ package com.example.ticon.activities;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import com.example.ticon.R;
 
@@ -12,5 +15,18 @@ public class DetailsActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_details);
+
+        Button changeToPreview = findViewById(R.id.previewButton);
+        changeToPreview.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                changeActivityPreview();
+            }
+        });
+    }
+
+    private void changeActivityPreview() {
+        Intent intent = new Intent(this, PreviewActivity.class);
+        startActivity(intent);
     }
 }
