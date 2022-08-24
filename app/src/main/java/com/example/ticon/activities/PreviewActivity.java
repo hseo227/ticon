0a99e5c8f603cae067b08b2030f74c176f196db2
ykim273 <ykim273@aucklanduni.ac.nz>
2022-08-24 20:30:58 +1200

    Declared variables in PreviewActivity

--- app/src/main/java/com/example/ticon/activities/PreviewActivity.java
+++ app/src/main/java/com/example/ticon/activities/PreviewActivity.java
@@ -5,31 +5,27 @@ import androidx.appcompat.widget.Toolbar;
 
 import android.os.Bundle;
 import android.view.View;
+import android.widget.Button;
 import android.widget.ImageView;
 
 import com.example.ticon.R;
 
 public class PreviewActivity extends AppCompatActivity {
 
-    public void changeChatEmo1 (View view){
-        ImageView previewEmoticon = (ImageView) findViewById(R.id.displayEmoticon);
-        previewEmoticon.setImageResource(R.drawable.bad_ducks3);
-    }
+//    public void changeChatEmo1 (View view){
+//        ImageView previewEmoticon = (ImageView) findViewById(R.id.displayEmoticon);
+//        previewEmoticon.setImageResource(R.drawable.bad_ducks3);
+//    }
 
-    public void changeChatEmo2 (View view){
-        ImageView previewEmoticon = (ImageView) findViewById(R.id.displayEmoticon);
-        previewEmoticon.setImageResource(R.drawable.bad_ducks6);
-    }
+    ImageView sentImage;
+    ImageView emoKeyImage1;
+    ImageView emoKeyImage2;
+    ImageView emoKeyImage3;
+    ImageView emoKeyImage4;
+    ImageView emoKeyImage5;
+    ImageView emoKeyImage6;
 
-    public void changeChatEmo3 (View view){
-        ImageView previewEmoticon = (ImageView) findViewById(R.id.displayEmoticon);
-        previewEmoticon.setImageResource(R.drawable.bad_ducks2);
-    }
-
-    public void changeChatEmo4 (View view){
-        ImageView previewEmoticon = (ImageView) findViewById(R.id.displayEmoticon);
-        previewEmoticon.setImageResource(R.drawable.bad_ducks2);
-    }
+    Button sendButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
