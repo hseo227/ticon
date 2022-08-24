eeca7c61a80a4cb7d38b28250f40d7400b6fe753
jjun273 <jjun273@aucklanduni.ac.nz>
2022-08-25 00:55:51 +1200

    Implemented searchView functionality.

--- app/src/main/java/com/example/ticon/activities/SearchActivity.java
+++ app/src/main/java/com/example/ticon/activities/SearchActivity.java
@@ -5,49 +5,100 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+//import android.widget.SearchView;
+import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
+import androidx.appcompat.widget.SearchView;
 
 import com.example.ticon.R;
+import com.example.ticon.data.DataProvider;
 import com.example.ticon.models.Emoticon;
+
+import org.w3c.dom.Text;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 public class SearchActivity extends AppCompatActivity {
+
+    private RecyclerView listRV;
+    private SearchView searchView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_search);
+        setContentView(R.layout.new_search);
+        listRV = findViewById(R.id.listRView);
+        searchView = findViewById(R.id.action_search);
 
-        RecyclerView listRV = findViewById(R.id.listRView);
+        TextView textView = (TextView) SearchActivity.this.findViewById(R.id.search_text);
 
-        // here we have created new array list and added data to it.
-        // Arraylist for storing data
-        List<Emoticon> listModelArrayList = new ArrayList<>();
+        Intent intent = getIntent();
+        String query = intent.getStringExtra("query");
 
-        // we are initializing our adapter class and passing our arraylist to it.
-        EmoticonAdapter emoticonAdapter = new EmoticonAdapter(this, listModelArrayList, "list");
+        if (query.equals("")){
+            textView.setText("Popular");
+            DataProvider.getAllData(emoticons -> {
+                EmoticonAdapter emoticonAdapter = new EmoticonAdapter(this, emoticons, "three");
+                getData(emoticonAdapter);
+            });
+            } else {
+            DataProvider.getAllData(emoticons -> {
+                List<Emoticon> resultEmoticons = getResults(emoticons, query);
+                if (resultEmoticons.isEmpty()) {
+                    textView.setText(R.string.no_results);
+                    textView.setVisibility(View.VISIBLE);
+                } else {
+                    textView.setText(R.string.search_results);
+                    textView.setVisibility(View.VISIBLE);
+                }
+                EmoticonAdapter emoticonAdapter = new EmoticonAdapter(this, resultEmoticons, "three");
+                getData(emoticonAdapter);
+            });
+        }
 
-        // below line is for setting a layout manager for our recycler view.
-        // here we are creating vertical list so we will provide orientation as vertical
-        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(this, LinearLayoutManager.VERTICAL, false);
+        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Intent newIntent = new Intent(getBaseContext(), SearchActivity.class);
+                newIntent.putExtra("query", query);
+                startActivity(newIntent);
+                return true;
+            }
+            @Override
+            public boolean onQueryTextChange(String query) {
+                return false;
+            }
+        });
 
-        // in below two lines we are setting layoutmanager and adapter to our recycler view.
-        listRV.setLayoutManager(linearLayoutManager);
-        listRV.setAdapter(emoticonAdapter);
-
-        ItemSpacing itemSpacing = new ItemSpacing(16);
-        listRV.addItemDecoration(itemSpacing);
 
     }
 
-    private void changeActivityDuck() {
-        Intent intent = new Intent(this, DetailsActivity.class);
-        startActivity(intent);
+    protected List<Emoticon> getResults(List<Emoticon> emoticons, String query) {
+        List<Emoticon> searchResults = new ArrayList<Emoticon>();
+        for (Emoticon emoticon : emoticons) {
+            String emoticonIgnoreCase = emoticon.getName().toLowerCase();
+            String queryIgnoreCase = query.toLowerCase();
+            if (emoticonIgnoreCase.contains(queryIgnoreCase)) {
+                searchResults.add(emoticon);
+            }
+        }
+        return searchResults;
+    }
+
+
+    protected void getData(EmoticonAdapter emoticonAdapter) {
+        // below line is for setting a layout manager for our recycler view.
+        // here we are creating vertical list so we will provide orientation as vertical
+        LinearLayoutManager layout = new LinearLayoutManager(this);
+        // in below two lines we are setting layoutmanager and adapter to our recycler view.
+        listRV.setLayoutManager(layout);
+        listRV.setAdapter(emoticonAdapter);
     }
 
 }
