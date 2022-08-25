23de4fa5a214445e2a39d68f1351d1b8a9346ef0
jjun273 <jjun273@aucklanduni.ac.nz>
2022-08-25 18:54:18 +1200

    Modified searchview to work inside toolbar. Added back button.

--- app/src/main/java/com/example/ticon/activities/SearchActivity.java
+++ app/src/main/java/com/example/ticon/activities/SearchActivity.java
@@ -1,7 +1,12 @@
 package com.example.ticon.activities;
 
+import android.app.SearchManager;
+import android.content.ComponentName;
+import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuItem;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
@@ -10,6 +15,8 @@ import android.widget.TextView;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.widget.Toolbar;
+import androidx.core.view.MenuItemCompat;
 import androidx.recyclerview.widget.LinearLayoutManager;
 import androidx.recyclerview.widget.RecyclerView;
 import androidx.appcompat.widget.SearchView;
@@ -32,55 +39,52 @@ public class SearchActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.new_search);
-        listRV = findViewById(R.id.listRView);
-        searchView = findViewById(R.id.action_search);
-
-        TextView textView = (TextView) SearchActivity.this.findViewById(R.id.search_text);
-
         Intent intent = getIntent();
         String query = intent.getStringExtra("query");
 
+        if (Intent.ACTION_SEARCH.equals(intent.getAction())) {
+            query = intent.getStringExtra(SearchManager.QUERY);
+            //use the query to search your data somehow
+        }
+
+        setContentView(R.layout.search_app_bar_list);
+        listRV = findViewById(R.id.listRView);
+        searchView = findViewById(R.id.action_search);
+
+        Toolbar myToolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(myToolbar);
+        getSupportActionBar().setHomeButtonEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        TextView textView = (TextView) SearchActivity.this.findViewById(R.id.search_text);
+
+
         if (query.equals("")){
-            searchView.setIconified(false);
-            searchView.setIconifiedByDefault(false);
+//            searchView.setIconified(false);
+//            searchView.setIconifiedByDefault(false);
             textView.setText(R.string.popular);
             DataProvider.getAllData(emoticons -> {
                 EmoticonAdapter emoticonAdapter = new EmoticonAdapter(this, emoticons, "three");
                 getData(emoticonAdapter);
             });
         } else {
-            searchView.setIconifiedByDefault(false);
-            searchView.setFocusable(false);
-            searchView.clearFocus();
+//            searchView.setIconifiedByDefault(false);
+//            searchView.setFocusable(false);
+//            searchView.clearFocus();
+            String finalQuery = query;
             DataProvider.getAllData(emoticons -> {
-                List<Emoticon> resultEmoticons = getResults(emoticons, query);
+                List<Emoticon> resultEmoticons = getResults(emoticons, finalQuery);
                 if (resultEmoticons.isEmpty()) {
                     textView.setText(R.string.no_results);
                     textView.setVisibility(View.VISIBLE);
                 } else {
-                    textView.setText(getString(R.string.search_results) + " \"" + query + "\"");
+                    textView.setText(getString(R.string.search_results) + " \"" + finalQuery + "\"");
                     textView.setVisibility(View.VISIBLE);
                 }
                 EmoticonAdapter emoticonAdapter = new EmoticonAdapter(this, resultEmoticons, "three");
                 getData(emoticonAdapter);
             });
         }
-
-        searchView.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
-            @Override
-            public boolean onQueryTextSubmit(String query) {
-                Intent newIntent = new Intent(getBaseContext(), SearchActivity.class);
-                newIntent.putExtra("query", query);
-                startActivity(newIntent);
-                return true;
-            }
-            @Override
-            public boolean onQueryTextChange(String query) {
-                return false;
-            }
-        });
-
 
     }
 
@@ -104,6 +108,51 @@ public class SearchActivity extends AppCompatActivity {
         // in below two lines we are setting layoutmanager and adapter to our recycler view.
         listRV.setLayoutManager(layout);
         listRV.setAdapter(emoticonAdapter);
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main_menu, menu);
+        SearchView search = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+        // Associate searchable configuration with the SearchView
+        SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
+        search.setSearchableInfo(searchManager.getSearchableInfo(new ComponentName(this, SearchActivity.class)));
+        search.setQueryHint(getResources().getString(R.string.search_hint));
+
+        search.setOnQueryTextListener(new SearchView.OnQueryTextListener() {
+            @Override
+            public boolean onQueryTextSubmit(String query) {
+                Intent newIntent = new Intent(getBaseContext(), SearchActivity.class);
+                newIntent.putExtra("query", query);
+                startActivity(newIntent);
+                return true;
+            }
+            @Override
+            public boolean onQueryTextChange(String query) {
+                return false;
+            }
+        });
+        return true;
+    }
+
+    @Override
+    public void onBackPressed() {
+        Intent intent = new Intent(this, MainActivity.class);
+        startActivity(intent);
+        finish();
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        if (item.getItemId() == R.id.action_search) {
+            return true;
+        } else if (item.getItemId() == R.id.favorite) {
+            return true;
+        } else {
+            onBackPressed();
+            return true;
+        }
     }
 
 }
