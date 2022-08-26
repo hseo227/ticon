53e348756f5d0db100e9a9a2a8f83c5d7fa78844
Hayoon Seo <hseo227@aucklanduni.ac.nz>
2022-08-27 04:49:43 +1200

    Refactored unused code in fragments.

--- app/src/main/java/com/example/ticon/activities/ui/myEmoticons/MyEmoticonsFragment.java
+++ app/src/main/java/com/example/ticon/activities/ui/myEmoticons/MyEmoticonsFragment.java
@@ -7,51 +7,25 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.ticon.R;
-import com.example.ticon.activities.MyEmoticonAdapter;
-import com.example.ticon.activities.SavedModel;
-
-import java.util.ArrayList;
+import com.example.ticon.databinding.FragmentSlideshowBinding;
 
 public class MyEmoticonsFragment extends Fragment {
 
-//    private FragmentSlideshowBinding binding;
-    private ArrayList<SavedModel> savedModelArrayList;
-
+    private FragmentSlideshowBinding binding;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
-//        SlideshowViewModel slideshowViewModel =
-//                new ViewModelProvider(this).get(SlideshowViewModel.class);
-//
-//        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
-//        View root = binding.getRoot();
-//
-//        final TextView textView = binding.textSlideshow;
-//        slideshowViewModel.getText().observe(getViewLifecycleOwner(), textView::setText);
-//        return root;
-        View view = inflater.inflate(R.layout.activity_search, container, false);
-        RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.listRView);
 
-        savedModelArrayList = new ArrayList<>();
-        savedModelArrayList.add(new SavedModel(R.drawable.crazy_dog1, R.drawable.ic_favorite_pink, "Crazy Dog", "Hoo"));
-        savedModelArrayList.add(new SavedModel(R.drawable.princess_moments1, R.drawable.ic_favorite_pink, "Princess Moments", "JK"));
-        savedModelArrayList.add(new SavedModel(R.drawable.stockman_monkey1, R.drawable.ic_favorite_pink, "Stockman Monkey", "JK"));
+        binding = FragmentSlideshowBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-        MyEmoticonAdapter myEmoticonAdapter = new MyEmoticonAdapter(getContext(), savedModelArrayList);
-        recyclerView.setAdapter(myEmoticonAdapter);
-        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-        recyclerView.setLayoutManager(layoutManager);
-
-        return view;
+        return root;
     }
 
-//    @Override
-//    public void onDestroyView() {
-//        super.onDestroyView();
-//        binding = null;
-//    }
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 }
--- app/src/main/java/com/example/ticon/activities/ui/wishlist/WishlistFragment.java
+++ app/src/main/java/com/example/ticon/activities/ui/wishlist/WishlistFragment.java
@@ -1,7 +1,5 @@
 package com.example.ticon.activities.ui.wishlist;
 
-import android.app.Activity;
-import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.View;
@@ -9,56 +7,26 @@ import android.view.ViewGroup;
 
 import androidx.annotation.NonNull;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
 
-import com.example.ticon.R;
-import com.example.ticon.activities.EmoticonAdapter;
-import com.example.ticon.activities.MyEmoticonAdapter;
-import com.example.ticon.activities.SavedModel;
-import com.example.ticon.data.DataProvider;
-//import com.example.ticon.databinding.FragmentGalleryBinding;
-
-import java.util.ArrayList;
+import com.example.ticon.databinding.FragmentGalleryBinding;
 
 public class WishlistFragment extends Fragment {
 
-        //    private FragmentGalleryBinding binding;
-        private ArrayList<SavedModel> savedModelArrayList;
-        private RecyclerView SavedListRV;
+    private FragmentGalleryBinding binding;
 
-        public View onCreateView(@NonNull LayoutInflater inflater,
-                                 ViewGroup container, Bundle savedInstanceState) {
+    public View onCreateView(@NonNull LayoutInflater inflater,
+                             ViewGroup container, Bundle savedInstanceState) {
 
-            View view = inflater.inflate(R.layout.activity_search, container, false);
-            RecyclerView recyclerView = (RecyclerView) view.findViewById(R.id.listRView);
+        binding = FragmentGalleryBinding.inflate(inflater, container, false);
+        View root = binding.getRoot();
 
-//        MyEmoticonAdapter myEmoticonAdapter = new MyEmoticonAdapter(getContext(), savedModelArrayList);
-//        recyclerView.setAdapter(myEmoticonAdapter);
-//        RecyclerView.LayoutManager layoutManager = new LinearLayoutManager(getActivity());
-//        recyclerView.setLayoutManager(layoutManager);
+        return root;
+    }
 
-            DataProvider.getAllData(emoticons -> {
-                // we are initializing our adapter class and passing our arraylist to it.
-                EmoticonAdapter emoticonAdapter = new EmoticonAdapter(getContext(), emoticons, "sidebar_list");
-                getData(emoticonAdapter);
-            });
+    @Override
+    public void onDestroyView() {
+        super.onDestroyView();
+        binding = null;
+    }
 
-            return view;
-        }
-
-        protected void getData(EmoticonAdapter emoticonAdapter) {
-            // below line is for setting a layout manager for our recycler view.
-            // here we are creating vertical list so we will provide orientation as vertical
-            LinearLayoutManager layout = new LinearLayoutManager(getActivity());
-            // in below two lines we are setting layoutmanager and adapter to our recycler view.
-            SavedListRV.setLayoutManager(layout);
-            SavedListRV.setAdapter(emoticonAdapter);
-        }
-
-//    @Override
-//    public void onDestroyView() {
-//        super.onDestroyView();
-//        binding = null;
-//    }
 }
