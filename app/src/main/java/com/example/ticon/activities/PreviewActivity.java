7fed8a6f8dd18edb58d7de8dd37a15c813d0d607
ykim273 <ykim273@aucklanduni.ac.nz>
2022-08-27 22:47:27 +1200

    Implement more display details in preview and details

--- app/src/main/java/com/example/ticon/activities/PreviewActivity.java
+++ app/src/main/java/com/example/ticon/activities/PreviewActivity.java
@@ -10,6 +10,7 @@ import android.view.WindowManager;
 import android.widget.Button;
 import android.widget.ImageButton;
 import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.RelativeLayout;
 
 import com.example.ticon.R;
@@ -20,6 +21,7 @@ public class PreviewActivity extends DetailsActivity implements View.OnClickList
     Emoticon emoticon;
 
     ImageView sentImage;
+    ImageView previewImage;
     ImageButton emoKeyButton1;
     ImageButton emoKeyButton2;
     ImageButton emoKeyButton3;
@@ -28,7 +30,11 @@ public class PreviewActivity extends DetailsActivity implements View.OnClickList
     ImageButton emoKeyButton6;
     ImageButton sendButton;
 
+    ImageButton exitTransparentButton;
+
     RelativeLayout chatBubble;
+    LinearLayout transparentLayout;
+
     int imageIndicator = 0;
 
     @Override
@@ -48,6 +54,7 @@ public class PreviewActivity extends DetailsActivity implements View.OnClickList
         // Create objects from layout
 
         sentImage = findViewById(R.id.displayEmoticon);
+        previewImage = findViewById(R.id.transparentEmoticon);
         emoKeyButton1 = findViewById(R.id.emoButton1);
         emoKeyButton2 = findViewById(R.id.emoButton2);
         emoKeyButton3 = findViewById(R.id.emoButton3);
@@ -56,8 +63,11 @@ public class PreviewActivity extends DetailsActivity implements View.OnClickList
         emoKeyButton6 = findViewById(R.id.emoButton6);
 
         sendButton = findViewById(R.id.sendButton);
+        exitTransparentButton = findViewById(R.id.exitTransparent);
 
         chatBubble = findViewById(R.id.chatLayout);
+        transparentLayout = findViewById(R.id.transparentDisplay);
+
 
         // Set ImageView objects to images from emoticon
 
@@ -75,6 +85,8 @@ public class PreviewActivity extends DetailsActivity implements View.OnClickList
         emoKeyButton4.setOnClickListener(this);
         emoKeyButton5.setOnClickListener(this);
         emoKeyButton6.setOnClickListener(this);
+        exitTransparentButton.setOnClickListener(this::exitTransparent);
+        sendButton.setOnClickListener(this::sendEmoticon);
     }
 
 
@@ -106,8 +118,19 @@ public class PreviewActivity extends DetailsActivity implements View.OnClickList
                 break;
         }
 
-        chatBubble.setVisibility(View.VISIBLE);
-        sentImage.setImageResource(getEmoticonId(imageIndicator));
+        transparentLayout.setVisibility(View.VISIBLE);
+        previewImage.setImageResource(getEmoticonId(imageIndicator));
     }
 
+    public void exitTransparent(View v){
+        transparentLayout.setVisibility(View.INVISIBLE);
+    }
+
+    public void sendEmoticon(View v) {
+        if (transparentLayout.getVisibility() == View.VISIBLE) {
+            transparentLayout.setVisibility(View.INVISIBLE);
+            chatBubble.setVisibility(View.VISIBLE);
+            sentImage.setImageResource(getEmoticonId(imageIndicator));
+        }
+    }
 }
