f9aa12c1f7460f65d6a485135d481a9cb7e9c926
jjun273 <jjun273@aucklanduni.ac.nz>
2022-08-28 00:00:42 +1200

    Documentation of activity classes.

--- app/src/main/java/com/example/ticon/activities/SearchActivity.java
+++ app/src/main/java/com/example/ticon/activities/SearchActivity.java
@@ -36,6 +36,7 @@ public class SearchActivity extends AppCompatActivity {
 
     private RecyclerView listRV;
 
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -75,9 +76,8 @@ public class SearchActivity extends AppCompatActivity {
             EmoticonAdapter emoticonAdapter = new EmoticonAdapter(this, resultEmoticons, "three");
             getData(emoticonAdapter);
         });
-
-
     }
+
 
     protected List<Emoticon> getResults(List<Emoticon> emoticons, String query) {
         // Search if query is included in emoticon - name, artist, category
@@ -85,12 +85,15 @@ public class SearchActivity extends AppCompatActivity {
         for (Emoticon emoticon : emoticons) {
             // Ignore upper/lower case in query
             String queryIgnoreCase = query.toLowerCase();
+
             // Ignore upper/lower case in emoticon database
             String emoticonIgnoreCase = emoticon.getName().toLowerCase();
             String emoticonArtistIgnoreCase = emoticon.getArtist().toLowerCase();
             String emoticonCategoryIgnoreCase = emoticon.getCategory().toLowerCase();
             // Ignore white space
+
             String emoticonIgnoreScore = emoticonIgnoreCase.replaceAll(" ", "");
+
             if (emoticonIgnoreScore.contains(queryIgnoreCase) || emoticonArtistIgnoreCase.contains(queryIgnoreCase) || emoticonCategoryIgnoreCase.contains(queryIgnoreCase)) {
                 searchResults.add(emoticon);
             }
@@ -103,16 +106,19 @@ public class SearchActivity extends AppCompatActivity {
         // below line is for setting a layout manager for our recycler view.
         // here we are creating vertical list so we will provide orientation as vertical
         LinearLayoutManager layout = new LinearLayoutManager(this);
+
         // in below two lines we are setting layoutmanager and adapter to our recycler view.
         listRV.setLayoutManager(layout);
         listRV.setAdapter(emoticonAdapter);
     }
+
 
     @Override
     public boolean onCreateOptionsMenu(Menu menu) {
         // Inflate the menu; this adds items to the action bar if it is present.
         getMenuInflater().inflate(R.menu.main_menu, menu);
         SearchView search = (SearchView) MenuItemCompat.getActionView(menu.findItem(R.id.action_search));
+
         // Associate searchable configuration with the SearchView
         SearchManager searchManager = (SearchManager) getSystemService(Context.SEARCH_SERVICE);
         search.setSearchableInfo(searchManager.getSearchableInfo(new ComponentName(this, SearchActivity.class)));
@@ -134,6 +140,7 @@ public class SearchActivity extends AppCompatActivity {
         return true;
     }
 
+
     @Override
     public void onBackPressed() {
         // Navigate to home
@@ -142,12 +149,14 @@ public class SearchActivity extends AppCompatActivity {
         finish();
     }
 
+
     public void onWishlistPressed() {
         // Navigate to wishlist
         Intent intent = new Intent(this, WishlistActivity.class);
         startActivity(intent);
         finish();
     }
+
 
     @Override
     public boolean onOptionsItemSelected(MenuItem item) {
